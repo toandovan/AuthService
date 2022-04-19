822777661e236f895c4a318883b1b965ce807301
ANSI <toan.do.khmtk17@hcmut.edu.vn>
2022-04-19 07:07:59 +0700

    change

--- src/main/java/com/example/authservice/AuthServiceApplication.java
+++ src/main/java/com/example/authservice/AuthServiceApplication.java
@@ -3,25 +3,17 @@ package com.example.authservice;
 import com.example.authservice.controller.AuthController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.GetMapping;
-import com.example.authservice.repository.UserRepository;
 import com.example.authservice.service.UserService;
 
 @SpringBootApplication
 @EnableEurekaClient
-@ComponentScan(basePackageClasses = {AuthController.class, UserService.class, UserRepository.class})
+@ComponentScan("com.example")
+@EntityScan("com.example")
 public class AuthServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthServiceApplication.class, args);
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
